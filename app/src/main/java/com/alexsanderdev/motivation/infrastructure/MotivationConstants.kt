85591529dfe56a0581751d053136eb9da64489a5
Alexsander Fernandes <afgoueia90@gmail.com>
2023-11-06 17:30:56 +0000

    Project finished.

--- app/src/main/java/com/alexsanderdev/motivation/infrastructure/MotivationConstants.kt
+++ app/src/main/java/com/alexsanderdev/motivation/infrastructure/MotivationConstants.kt
@@ -1,7 +1,19 @@
+/*
+ * Copyright (c) 2023. Created by Alexsander at 11/6. All rights reserved.
+ * GitHub: https://github.com/alexsanderfer/
+ * Portfolio: https://alexsanderfer.netlify.app/
+ */
+
 package com.alexsanderdev.motivation.infrastructure
 
 class MotivationConstants private constructor() {
     object KEY {
         const val USER_NAME = "USER_NAME"
+    }
+
+    object FILTER {
+        const val ALL = 1
+        const val HAPPY = 2
+        const val SUNNY = 3
     }
 }
--- app/src/main/java/com/alexsanderdev/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/alexsanderdev/motivation/ui/MainActivity.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023. Created by Alexsander at 11/6. All rights reserved.
+ * GitHub: https://github.com/alexsanderfer/
+ * Portfolio: https://alexsanderfer.netlify.app/
+ */
+
 package com.alexsanderdev.motivation.ui
 
 import android.os.Bundle
@@ -5,6 +11,7 @@ import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.core.content.ContextCompat
 import com.alexsanderdev.motivation.R
+import com.alexsanderdev.motivation.data.Mock
 import com.alexsanderdev.motivation.databinding.ActivityMainBinding
 import com.alexsanderdev.motivation.infrastructure.MotivationConstants
 import com.alexsanderdev.motivation.infrastructure.SecurityPreferences
@@ -12,13 +19,17 @@ import com.alexsanderdev.motivation.infrastructure.SecurityPreferences
 class MainActivity : ComponentActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private var categoryID = MotivationConstants.FILTER.ALL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         handeUserName()
+        handleFilter(R.id.image_all)
+        handleNextPhrase()
 
         // Eventos
         binding.buttonNewPhrase.setOnClickListener(this)
@@ -30,17 +41,38 @@ class MainActivity : ComponentActivity(), View.OnClickListener {
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phrase) {
-            var s = ""
+            handleNextPhrase()
         } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)) {
             handleFilter(view.id)
         }
     }
 
+    private fun handleNextPhrase() {
+        binding.textPhrase.text = Mock().getPhrase(categoryID)
+    }
+
     private fun handleFilter(id: Int) {
 
-        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
 
-        if (id == R.id.image_all) {
+        when (id) {
+            R.id.image_all -> {
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryID = MotivationConstants.FILTER.ALL
+            }
+
+            R.id.image_happy -> {
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryID = MotivationConstants.FILTER.HAPPY
+            }
+
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryID = MotivationConstants.FILTER.SUNNY
+            }
+
         }
     }
 
